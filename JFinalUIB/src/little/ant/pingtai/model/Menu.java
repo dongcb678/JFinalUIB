a1640a976a3782ebfab4f58b65e53b3e03d7d283
Administrator <Administrator@PC-DONGHUAJIAN.hikvision.com>
2014-06-04 14:14:13 +0800

    关联数据查询

--- JFinalUIB/src/little/ant/pingtai/model/Menu.java
+++ JFinalUIB/src/little/ant/pingtai/model/Menu.java
@@ -8,4 +8,12 @@ public class Menu extends Model<Menu> {
 	
 	public static final Menu dao = new Menu();
 	
+	/**
+	 * 根据菜单获取对应的功能，可能为空
+	 * @return
+	 */
+	public Operator getOperator(){
+		return Operator.dao.findById(get("operatorids"));
+	}
+	
 }
--- JFinalUIB/src/little/ant/pingtai/model/User.java
+++ JFinalUIB/src/little/ant/pingtai/model/User.java
@@ -8,4 +8,28 @@ public class User extends Model<User> {
 	
 	public static final User dao = new User();
 	
+	/**
+	 * 关联查询，获取用户详细信息
+	 * @return
+	 */
+	public UserInfo getUserInfo(){
+		return UserInfo.dao.findById(get("userinfoids"));
+	}
+
+	/**
+	 * 关联查询，获取用户部门信息
+	 * @return
+	 */
+	public Department getDepartment(){
+		return Department.dao.findById(get("departmentids"));
+	}
+
+	/**
+	 * 关联查询，获取用户岗位信息
+	 * @return
+	 */
+	public Station getStation(){
+		return Station.dao.findById(get("stationids"));
+	}
+	
 }
