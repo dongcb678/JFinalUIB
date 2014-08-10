6d8623ab8a35cd753fdd93c8157ed1378e9abf67
dongcb678 <dongcb678@163.com>
2014-08-10 21:04:15 +0800

    字典标签处理

---
+++ JFinalUIB/src/little/ant/pingtai/beetl/tag/DictTag.java
@@ -0,0 +1,199 @@
+package little.ant.pingtai.beetl.tag;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import little.ant.pingtai.common.DictKeys;
+import little.ant.pingtai.model.BaseModel;
+import little.ant.pingtai.model.Dict;
+import little.ant.pingtai.thread.ThreadParamInit;
+
+import org.apache.log4j.Logger;
+import org.beetl.core.Tag;
+
+import com.jfinal.plugin.ehcache.CacheKit;
+
+/**
+ * 字典select
+ * 
+ * @author 董华健
+ */
+public class DictTag extends Tag {
+
+	private static Logger log = Logger.getLogger(DictTag.class);
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void render() {
+		try {
+			Map<String, String> param = (Map<String, String>) args[1];
+
+			log.debug("获取标签属性");
+
+			String type = param.get("type") == null ? "" : param.get("type");
+			
+			String id = param.get("id") == null ? "" : param.get("id");
+			String name = param.get("name") == null ? "" : param.get("name");
+			
+			String classs = param.get("classs") == null ? "" : param.get("classs");
+			String style = param.get("style") == null ? "" : param.get("style");
+
+			String number = param.get("number") == null ? "" : param.get("number");
+			String defaultnumber = param.get("defaultnumber") == null ? "" : param.get("defaultnumber");
+
+			log.debug("字典标签");
+			
+			if(type.equals("")){
+				ctx.byteWriter.writeString(select(id, name, classs, style, number, defaultnumber));
+				
+			}else if(type.equals("select")){
+				ctx.byteWriter.writeString(select(id, name, classs, style, number, defaultnumber));
+				
+			}else if(type.equals("radio")){
+				ctx.byteWriter.writeString(radio(id, name, classs, style, number, defaultnumber));
+				
+			}else if(type.equals("checkbox")){
+				ctx.byteWriter.writeString(checkbox(id, name, classs, style, number, defaultnumber));
+				
+			} else {
+				ctx.byteWriter.writeString(select(id, name, classs, style, number, defaultnumber));
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 下拉标签
+	 * @param id
+	 * @param name
+	 * @param classs
+	 * @param style
+	 * @param number
+	 * @param defaultnumber
+	 * @return
+	 */
+	private String select(String id, String name, String classs, String style, String number, String defaultnumber){
+		StringBuilder sb = new StringBuilder();
+		if (null != id && !id.isEmpty()) {
+			sb.append("<select id=\"").append(id).append("\" name=\"").append(name);
+			sb.append("\" class=\"").append(classs).append("\" style=\"").append(style).append("\" >");
+		} else {
+			sb.append("<select name=\"").append(name).append("\" class=\"").append(classs);
+			sb.append("\" style=\"").append(style).append("\" >");
+		}
+
+		Dict parentDict = CacheKit.get(DictKeys.cache_name_system, ThreadParamInit.cacheStart_dict + number);
+		String parentI18n = parentDict.getStr("i18n");
+		
+		String val = "val";
+		if(null != parentI18n && parentI18n.equals("1")){
+			String localePram = (String) ctx.getGlobal("localePram");
+			val += BaseModel.i18n(localePram);
+		}
+		List<Dict> dictList = CacheKit.get(DictKeys.cache_name_system, ThreadParamInit.cacheStart_dict_child + number);
+
+		for (Dict dict : dictList) {
+			String numbersTemp = dict.getStr("numbers");
+			String namesTemp = dict.getStr("names");
+			String valueTemp = dict.getStr(val);
+			if (null != defaultnumber && null != valueTemp && defaultnumber.equals(numbersTemp)) {// 默认选中
+				sb.append("<option value=\"").append(valueTemp).append("\" selected=\"selected\">");
+				sb.append(namesTemp);
+				sb.append("</option>");
+
+			} else {
+				sb.append("<option value=\"").append(valueTemp).append("\">");
+				sb.append(namesTemp);
+				sb.append("</option>");
+			}
+		}
+		sb.append("</select>");
+		
+		return sb.toString();
+	}
+
+	/**
+	 * 单选
+	 * @param id
+	 * @param name
+	 * @param classs
+	 * @param style
+	 * @param number
+	 * @param defaultnumber
+	 * @return
+	 */
+	private String radio(String id, String name, String classs, String style, String number, String defaultnumber){
+		StringBuilder sb = new StringBuilder();
+		
+		Dict parentDict = CacheKit.get(DictKeys.cache_name_system, ThreadParamInit.cacheStart_dict + number);
+		String parentI18n = parentDict.getStr("i18n");
+		
+		String val = "val";
+		if(null != parentI18n && parentI18n.equals("1")){
+			String localePram = (String) ctx.getGlobal("localePram");
+			val += BaseModel.i18n(localePram);
+		}
+		List<Dict> dictList = CacheKit.get(DictKeys.cache_name_system, ThreadParamInit.cacheStart_dict_child + number);
+
+		for (Dict dict : dictList) {
+			String numbersTemp = dict.getStr("numbers");
+			String namesTemp = dict.getStr("names");
+			String valueTemp = dict.getStr(val);
+			if (null != defaultnumber && null != valueTemp && defaultnumber.equals(numbersTemp)) {// 默认选中
+				sb.append(namesTemp).append("<input type=\"radio\" id=\"").append(id).append(dictList.indexOf(dict)).append("\" name=\"").append(name)
+				.append("\" value=\"").append(valueTemp).append("\" class=\"").append(classs).append("\" style=\"").append(style).append("\" checked=\"checked\" >");
+				
+			} else {
+				sb.append(namesTemp).append("<input type=\"radio\" id=\"").append(id).append(dictList.indexOf(dict)).append("\" name=\"").append(name)
+				.append("\" value=\"").append(valueTemp).append("\" class=\"").append(classs).append("\" style=\"").append(style).append("\" >");
+			}
+		}
+		sb.append("</select>");
+		
+		return sb.toString();
+	}
+
+	/**
+	 * 多选
+	 * @param id
+	 * @param name
+	 * @param classs
+	 * @param style
+	 * @param number
+	 * @param defaultnumber
+	 * @return
+	 */
+	private String checkbox(String id, String name, String classs, String style, String number, String defaultnumber){
+		StringBuilder sb = new StringBuilder();
+		
+		Dict parentDict = CacheKit.get(DictKeys.cache_name_system, ThreadParamInit.cacheStart_dict + number);
+		String parentI18n = parentDict.getStr("i18n");
+		
+		String val = "val";
+		if(null != parentI18n && parentI18n.equals("1")){
+			String localePram = (String) ctx.getGlobal("localePram");
+			val += BaseModel.i18n(localePram);
+		}
+		List<Dict> dictList = CacheKit.get(DictKeys.cache_name_system, ThreadParamInit.cacheStart_dict_child + number);
+
+		for (Dict dict : dictList) {
+			String numbersTemp = dict.getStr("numbers");
+			String namesTemp = dict.getStr("names");
+			String valueTemp = dict.getStr(val);
+			if (null != defaultnumber && null != valueTemp && defaultnumber.equals(numbersTemp)) {// 默认选中
+				sb.append(namesTemp).append("<input type=\"checkbox\" id=\"").append(id).append(dictList.indexOf(dict)).append("\" name=\"").append(name)
+				.append("\" value=\"").append(valueTemp).append("\" class=\"").append(classs).append("\" style=\"").append(style).append("\" checked=\"checked\" >");
+				
+			} else {
+				sb.append(namesTemp).append("<input type=\"checkbox\" id=\"").append(id).append(dictList.indexOf(dict)).append("\" name=\"").append(name)
+				.append("\" value=\"").append(valueTemp).append("\" class=\"").append(classs).append("\" style=\"").append(style).append("\" >");
+			}
+		}
+		sb.append("</select>");
+		
+		return sb.toString();
+	}
+}
--- JFinalUIB/src/little/ant/pingtai/run/JfinalConfig.java
+++ JFinalUIB/src/little/ant/pingtai/run/JfinalConfig.java
@@ -4,7 +4,7 @@ import little.ant.pingtai.beetl.func.EscapeXml;
 import little.ant.pingtai.beetl.func.HasPrivilegeUrl;
 import little.ant.pingtai.beetl.func.OrderBy;
 import little.ant.pingtai.beetl.render.MyBeetlRenderFactory;
-import little.ant.pingtai.beetl.tag.DictSelect;
+import little.ant.pingtai.beetl.tag.DictTag;
 import little.ant.pingtai.common.DictKeys;
 import little.ant.pingtai.handler.GlobalHandler;
 import little.ant.pingtai.interceptor.AuthenticationInterceptor;
@@ -64,7 +64,7 @@ public class JfinalConfig extends JFinalConfig {
 		groupTemplate.registerFunction("hasPrivilegeUrl", new HasPrivilegeUrl());
 		groupTemplate.registerFunction("orderBy", new OrderBy());
 		groupTemplate.registerFunction("escapeXml", new EscapeXml());
-		groupTemplate.registerTag("dictSelect", DictSelect.class);
+		groupTemplate.registerTag("dict", DictTag.class);
 		
 		log.info("configConstant 视图error page设置");
 		me.setError401View("/common/401.html");
