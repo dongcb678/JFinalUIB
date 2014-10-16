5ce52f17e75069fe9b5999552afa0aa5747aabdd
Administrator <Administrator@PC-DONGHUAJIAN.hikvision.com>
2014-10-16 14:39:56 +0800

    修复国际化资源文件加载bug，在不同容器中运行结果不一致问题

--- JFinalUIB/src/little/ant/pingtai/plugin/I18NPlugin.java
+++ JFinalUIB/src/little/ant/pingtai/plugin/I18NPlugin.java
@@ -1,10 +1,11 @@
 package little.ant.pingtai.plugin;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Enumeration;
 import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
-import java.util.ResourceBundle;
+import java.util.Properties;
 
 import com.jfinal.log.Logger;
 import com.jfinal.plugin.IPlugin;
@@ -18,14 +19,17 @@ public class I18NPlugin implements IPlugin {
     protected final Logger log = Logger.getLogger(getClass());
 
 	private static final Map<String, Map<String, String>> resourceBundleMap = new HashMap<String, Map<String, String>>();
-
-	private static final String[] languages = {
-		"zh", "zh_cn", "zh_hk", "zh_tw",
-		"en", "en_us",
-		"ja"
-	};
 	
 	public static Map<String, String> get(String localePramKey){
+		localePramKey = localePramKey.toLowerCase();
+		
+		if(localePramKey.equals("zh")){
+			localePramKey = "zh_cn";
+			
+		}else if(localePramKey.equals("en")){
+			localePramKey = "en_us";
+		}
+		
 		Map<String, String> map = resourceBundleMap.get(localePramKey);
 		if(map != null){
 			return map;
@@ -36,30 +40,41 @@ public class I18NPlugin implements IPlugin {
 	
 	@Override
 	public boolean start() {
+		String[] languages = {
+				"zh_CN", 
+				"zh_HK", 
+				"zh_TW",
+				"en_US",
+				"ja"
+			};
+		
 		for (String language : languages) {
-			String country = null;
-			Locale locale = null;
-			if(language.equals("zh")){
-				country = "CN";
-				locale = new Locale(language, country);
-			
-			}else if(language.equals("en")){
-				country = "US";
-				locale = new Locale(language, country);
-			
-			}else{
-				locale = new Locale(language);
+			Properties properties = new Properties();
+			InputStream inputStream = null;
+			try {
+				//inputStream = this.getClass().getClassLoader().getResourceAsStream(filePath);
+				inputStream = I18NPlugin.class.getResourceAsStream("/message_" + language + ".properties");// "/init.properties"
+				properties.load(inputStream);
+				
+				Map<String, String> i18nMap = new HashMap<String, String>();
+				Enumeration<Object> keys = properties.keys();
+				while (keys.hasMoreElements()) {
+					String key = (String) keys.nextElement();
+					String value = properties.getProperty(key);
+					i18nMap.put(key, value);
+				}
+				resourceBundleMap.put(language.toLowerCase(), i18nMap);
+			} catch (Exception exception) {
+				log.info("加载properties失败！...");
+			} finally {
+				try {
+					if (inputStream != null) {
+						inputStream.close();
+					}
+				} catch (IOException e) {
+					log.info("加载properties后关闭失败！...");
+				}
 			}
-			
-			ResourceBundle resourceBundle = ResourceBundle.getBundle("message", locale);
-			Enumeration<String> keys = resourceBundle.getKeys();
-			Map<String, String> i18nMap = new HashMap<String, String>();
-			while (keys.hasMoreElements()) {
-				String key = (String) keys.nextElement();
-				String value = resourceBundle.getString(key);
-				i18nMap.put(key, value);
-			}
-			resourceBundleMap.put(language, i18nMap);
 		}
 		return true;
 	}
