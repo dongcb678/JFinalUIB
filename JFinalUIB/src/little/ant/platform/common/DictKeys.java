5fb45f651e784559858684c4f75b36f32c399e2b
Administrator <Administrator@PC-DONGHUAJIAN.hikvision.com>
2014-11-25 17:33:57 +0800

    字典类添加详细注释说明

--- JFinalUIB/src/little/ant/platform/common/DictKeys.java
+++ JFinalUIB/src/little/ant/platform/common/DictKeys.java
@@ -1,16 +1,11 @@
 package little.ant.platform.common;
 
-import org.apache.log4j.Logger;
-
 /**
  * 常量数据字典
  * @author 董华健
  */
 public abstract class DictKeys {
 
-	@SuppressWarnings("unused")
-	private static Logger log = Logger.getLogger(DictKeys.class);
-	
 	/**
 	 * URL缓存Key
 	 */
@@ -62,44 +57,119 @@ public abstract class DictKeys {
 	public static final String config_domain_key = "config.domain";
 
 	/**
-	 * mail 配置
+	 * mail 配置：邮件服务器地址
 	 */
 	public static final String config_mail_host = "config.mail.host";
+
+	/**
+	 * mail 配置：邮件服务器端口
+	 */
 	public static final String config_mail_port = "config.mail.port";
+
+	/**
+	 * mail 配置：邮件服务器账号
+	 */
 	public static final String config_mail_from = "config.mail.from";
+
+	/**
+	 * mail 配置：邮件服务器名称
+	 */
 	public static final String config_mail_userName = "config.mail.userName";
+
+	/**
+	 * mail 配置：邮件服务器密码
+	 */
 	public static final String config_mail_password = "config.mail.password";
-	public static final String config_mail_to = "config.mail.to"; // 接收邮件地址
+
+	/**
+	 * mail 配置：接收邮件地址
+	 */
+	public static final String config_mail_to = "config.mail.to";
 	
 	/**
 	 * 当前数据库类型
 	 */
 	public static final String db_type_key = "db.type";
-	
-	public static final String db_type_postgresql = "postgresql";
-	public static final String db_type_mysql = "mysql";
-	public static final String db_type_oracle = "oracle";
-	
-	public static final String db_connection_driverClass = "driverClass";
-	public static final String db_connection_jdbcUrl = "jdbcUrl";
-	public static final String db_connection_userName = "userName";
-	public static final String db_connection_passWord = "passWord";
-	
-	public static final String db_connection_ip = "db_ip";
-	public static final String db_connection_port = "db_port";
-	public static final String db_connection_dbName = "db_name";
 
-	public static final String db_initialSize = "db.initialSize";
-	public static final String db_minIdle = "db.minIdle";
-	public static final String db_maxActive = "db.maxActive";
-	
-	public static final String db_dataSource_main = "db.dataSource.main"; // 主数据源名称
+	/**
+	 * 当前数据库类型：postgresql
+	 */
+	public static final String db_type_postgresql = "postgresql";
+
+	/**
+	 * 当前数据库类型：mysql
+	 */
+	public static final String db_type_mysql = "mysql";
+
+	/**
+	 * 当前数据库类型：oracle
+	 */
+	public static final String db_type_oracle = "oracle";
+
+	/**
+	 * 数据库连接参数：驱动
+	 */
+	public static final String db_connection_driverClass = "driverClass";
 	
 	/**
-	 * 分页参数初始化值
+	 * 数据库连接参数：连接URL
 	 */
-	public static final int default_pageNumber = 1;// 第几页
-	public static final int default_pageSize = 20;// 每页显示几多
+	public static final String db_connection_jdbcUrl = "jdbcUrl";
+	
+	/**
+	 * 数据库连接参数：用户名
+	 */
+	public static final String db_connection_userName = "userName";
+	
+	/**
+	 * 数据库连接参数：密码
+	 */
+	public static final String db_connection_passWord = "passWord";
+
+	/**
+	 * 数据库连接参数：数据库服务器IP
+	 */
+	public static final String db_connection_ip = "db_ip";
+	
+	/**
+	 * 数据库连接参数：数据库服务器端口
+	 */
+	public static final String db_connection_port = "db_port";
+	
+	/**
+	 * 数据库连接参数：数据库名称
+	 */
+	public static final String db_connection_dbName = "db_name";
+
+	/**
+	 * 数据库连接池参数：初始化连接大小
+	 */
+	public static final String db_initialSize = "db.initialSize";
+
+	/**
+	 * 数据库连接池参数：最少连接数
+	 */
+	public static final String db_minIdle = "db.minIdle";
+
+	/**
+	 * 数据库连接池参数：最多连接数
+	 */
+	public static final String db_maxActive = "db.maxActive";
+
+	/**
+	 *  主数据源名称：系统主数据源
+	 */
+	public static final String db_dataSource_main = "db.dataSource.main";
+	
+	/**
+	 * 分页参数初始化值：默认显示第几页
+	 */
+	public static final int default_pageNumber = 1;
+	
+	/**
+	 * 分页参数初始化值：默认每页显示几多
+	 */
+	public static final int default_pageSize = 20;
 	
 	/**
 	 * 用户登录状态码
