4c19f0adbfbd7ee8bb7cffa16b568550328a23c1
Administrator <Administrator@PC-DONGHUAJIAN.hikvision.com>
2015-04-16 14:54:11 +0800

    tree封装类

---
+++ JFinalUIBV2/src/little/ant/platform/common/ZtreeNode.java
@@ -0,0 +1,60 @@
+package little.ant.platform.common;
+
+/**
+ * Ztree节点数据封装
+ * @author 董华健
+ *
+ */
+public class ZtreeNode {
+	
+	private String id;
+
+	private String name;
+
+	private boolean isParent;
+
+	private boolean checked;
+
+	private String icon;
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean getIsParent() {
+		return isParent;
+	}
+
+	public void setIsParent(boolean isParent) {
+		this.isParent = isParent;
+	}
+
+	public boolean isChecked() {
+		return checked;
+	}
+
+	public void setChecked(boolean checked) {
+		this.checked = checked;
+	}
+
+	public String getIcon() {
+		return icon;
+	}
+
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+	
+}
