1aadd36ea153410118369b34a38d3a9f896ee8be
Administrator <Administrator@DADI-20140326OR>
2015-04-22 19:42:39 +0800

    引入echarts

--- JFinalUIBV2/WebContent/jsFile/rapido/common/modals.js
+++ JFinalUIBV2/WebContent/jsFile/rapido/common/modals.js
@@ -5,8 +5,14 @@ var common_modals = function() {
 	 * 注册弹出框事件功能
 	 */
 	var initModals = function() {
+		var progressHtml = '<div class="loading-spinner" style="width: 200px; margin-left: -100px;">' + 
+		'<div class="progress progress-striped active">' + 
+		'<div class="progress-bar" style="width: 100%;"></div>' + '</div>' + '</div>';
+		
 		$.fn.modalmanager.defaults.resize = true;
-		$.fn.modal.defaults.spinner = $.fn.modalmanager.defaults.spinner = '<div class="loading-spinner" style="width: 200px; margin-left: -100px;">' + '<div class="progress progress-striped active">' + '<div class="progress-bar" style="width: 100%;"></div>' + '</div>' + '</div>';
+		$.fn.modal.defaults.spinner = $.fn.modalmanager.defaults.spinner = progressHtml;
+		
+		
 		var $modal = $('#ajax-modal');
 		/*
 		$('.ajaxDialog').on('click', function() {
