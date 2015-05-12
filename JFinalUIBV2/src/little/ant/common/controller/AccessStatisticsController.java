818ffd708fe2d16ce0d50aa7fcb510c918de1eb0
Administrator <Administrator@PC-DONGHUAJIAN.hikvision.com>
2015-05-12 17:00:28 +0800

    访问统计页面开发

--- JFinalUIBV2/src/little/ant/common/controller/AccessStatisticsController.java
+++ JFinalUIBV2/src/little/ant/common/controller/AccessStatisticsController.java
@@ -1,26 +1,18 @@
 package little.ant.common.controller;
 
+import little.ant.common.model.AccessStatistics;
+import little.ant.common.service.AccessStatisticsService;
 import little.ant.platform.annotation.Controller;
 import little.ant.platform.controller.BaseController;
-import little.ant.common.service.AccessStatisticsService;
-import little.ant.common.validator.AccessStatisticsValidator;
-import little.ant.common.model.AccessStatistics;
 
 import org.apache.log4j.Logger;
-
-import com.jfinal.aop.Before;
 
 /**
  * XXX 管理	
  * 描述：
  * 
  * /jf/common/accessStatistics
- * /jf/common/accessStatistics/save
- * /jf/common/accessStatistics/edit
- * /jf/common/accessStatistics/update
  * /jf/common/accessStatistics/view
- * /jf/common/accessStatistics/delete
- * /common/accessStatistics/add.html
  * 
  */
 @Controller(controllerKey = "/jf/common/accessStatistics")
@@ -38,48 +30,12 @@ public class AccessStatisticsController extends BaseController {
 	}
 	
 	/**
-	 * 保存
-	 */
-	@Before(AccessStatisticsValidator.class)
-	public void save() {
-		getModel(AccessStatistics.class).save();
-		render("/common/accessStatistics/add.html");
-	}
-	
-	/**
-	 * 准备更新
-	 */
-	public void edit() {
-		AccessStatistics accessStatistics = AccessStatistics.dao.findById(getPara());
-		setAttr("accessStatistics", accessStatistics);
-		render("/common/accessStatistics/update.html");
-	}
-	
-	/**
-	 * 更新
-	 */
-	@Before(AccessStatisticsValidator.class)
-	public void update() {
-		getModel(AccessStatistics.class).update();
-		redirect("/jf/common/accessStatistics");
-	}
-
-	/**
 	 * 查看
 	 */
 	public void view() {
 		AccessStatistics accessStatistics = AccessStatistics.dao.findById(getPara());
 		setAttr("accessStatistics", accessStatistics);
 		render("/common/accessStatistics/view.html");
-	}
-	
-	/**
-	 * 删除
-	 */
-	public void delete() {
-		String param = (getPara() == null ? ids : getPara());
-		AccessStatisticsService.service.delete(param);
-		redirect("/jf/common/accessStatistics");
 	}
 	
 }
--- JFinalUIBV2/src/little/ant/common/service/AccessStatisticsService.java
+++ JFinalUIBV2/src/little/ant/common/service/AccessStatisticsService.java
@@ -1,11 +1,8 @@
 package little.ant.common.service;
 
-import little.ant.common.model.AccessStatistics;
-import little.ant.common.model.IpBlacklist;
 import little.ant.platform.common.DictKeys;
 import little.ant.platform.common.SplitPage;
 import little.ant.platform.service.BaseService;
-import little.ant.platform.tools.ToolDateTime;
 
 import org.apache.log4j.Logger;
 
@@ -25,29 +22,4 @@ public class AccessStatisticsService extends BaseService {
 		splitPageBase(DictKeys.db_dataSource_main, splitPage, select, "common.accessStatistics.splitPage");
 	}
 
-	/**
-	 * 保存
-	 * @param accessStatistics
-	 * @param userIds
-	 * @return
-	 */
-	public String save(AccessStatistics accessStatistics, String userIds){
-		accessStatistics.set(IpBlacklist.colunm_createuser, userIds);
-		accessStatistics.set(IpBlacklist.colunm_createdate, ToolDateTime.getSqlTimestamp(null));
-		accessStatistics.set(IpBlacklist.colunm_isdelete, "0");
-		accessStatistics.save();
-		return accessStatistics.getPKValue();
-	}
-
-	/**
-	 * 删除
-	 * @param ids
-	 */
-	public void delete(String ids){
-		String[] idsArr = splitByComma(ids);
-		for (String id : idsArr) {
-			AccessStatistics.dao.deleteById(id);
-		}
-	}
-	
 }
