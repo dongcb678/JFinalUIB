72da28742d105dcbd6e7924abfe4f1685068e382
13995531140 <13995531140@163.com>
2015-05-29 17:32:11 +0800

    常量注释维护

--- JFinalUIBV2/platform/little/ant/platform/tools/ToolString.java
+++ JFinalUIBV2/platform/little/ant/platform/tools/ToolString.java
@@ -22,50 +22,149 @@ public abstract class ToolString {
 	private static Logger log = Logger.getLogger(ToolString.class);
 
 	/**
-	 * 常用正则表达式
+	 * 常用正则表达式：匹配非负整数（正整数 + 0）
 	 */
-	public final static String regExp_integer_1 = "^\\d+$"; // 匹配非负整数（正整数 + 0）
-	public final static String regExp_integer_2 = "^[0-9]*[1-9][0-9]*$"; // 匹配正整数
-	public final static String regExp_integer_3 = "^((-\\d+) ?(0+))$"; // 匹配非正整数（负整数  + 0）
-	public final static String regExp_integer_4 = "^-[0-9]*[1-9][0-9]*$"; // 匹配负整数
-	public final static String regExp_integer_5 = "^-?\\d+$"; // 匹配整数
-
-	public final static String regExp_float_1 = "^\\d+(\\.\\d+)?$"; // 匹配非负浮点数（正浮点数 + 0）
-	public final static String regExp_float_2 = "^(([0-9]+\\.[0-9]*[1-9][0-9]*) ?([0-9]*[1-9][0-9]*\\.[0-9]+) ?([0-9]*[1-9][0-9]*))$"; // 匹配正浮点数
-	public final static String regExp_float_3 = "^((-\\d+(\\.\\d+)?) ?(0+(\\.0+)?))$"; // 匹配非正浮点数（负浮点数 + 0）
-	public final static String regExp_float_4 = "^(-(([0-9]+\\.[0-9]*[1-9][0-9]*) ?([0-9]*[1-9][0-9]*\\.[0-9]+) ?([0-9]*[1-9][0-9]*)))$"; // 匹配负浮点数
-	public final static String regExp_float_5 = "^(-?\\d+)(\\.\\d+)?$"; // 匹配浮点数
-
-	public final static String regExp_letter_1 = "^[A-Za-z]+$";// 匹配由26个英文字母组成的字符串
-	public final static String regExp_letter_2 = "^[A-Z]+$";// 匹配由26个英文字母的大写组成的字符串
-	public final static String regExp_letter_3 = "^[a-z]+$";// 匹配由26个英文字母的小写组成的字符串
-	public final static String regExp_letter_4 = "^[A-Za-z0-9]+$";// 匹配由数字和26个英文字母组成的字符串
-	public final static String regExp_letter_5 = "^\\w+$";// 匹配由数字、26个英文字母或者下划线组成的字符串
-
-	public final static String regExp_email = "^[\\w-]+(\\.[\\w-]+)*@[\\w-]+(\\.[\\w-]+)+$"; // 匹配email地址
+	public final static String regExp_integer_1 = "^\\d+$";
 	
-	public final static String regExp_url_1 = "^[a-zA-z]+://(\\w+(-\\w+)*)(\\.(\\w+(-\\w+)*))*(\\?\\S*)?$"; // 匹配url
-	public final static String regExp_url_2 = "[a-zA-z]+://[^\\s]*"; // 匹配url
-		
-	public final static String regExp_chinese_1 = "[\\u4e00-\\u9fa5]"; // 匹配中文字符
-	public final static String regExp_chinese_2 = "[^\\x00-\\xff]"; // 匹配双字节字符(包括汉字在内)
-
-	public final static String regExp_line = "\\n[\\s ? ]*\\r"; // 匹配空行：
-
-	public final static String regExp_html_1 = "/ <(.*)>.* <\\/\\1> ? <(.*) \\/>/"; // 匹配HTML标记
-	public final static String regExp_startEndEmpty = "(^\\s*) ?(\\s*$)"; // 匹配首尾空格
-
-	public final static String regExp_accountNumber = "^[a-zA-Z][a-zA-Z0-9_]{4,15}$"; //匹配帐号是否合法(字母开头，允许5-16字节，允许字母数字下划线)
+	/**
+	 * 常用正则表达式：匹配正整数
+	 */
+	public final static String regExp_integer_2 = "^[0-9]*[1-9][0-9]*$";
 	
-	public final static String regExp_telephone = "\\d{3}-\\d{8} ?\\d{4}-\\d{7}"; //匹配国内电话号码，匹配形式如 0511-4405222 或 021-87888822 
+	/**
+	 * 常用正则表达式：匹配非正整数（负整数  + 0）
+	 */
+	public final static String regExp_integer_3 = "^((-\\d+) ?(0+))$";
 	
-	public final static String regExp_qq = "[1-9][0-9]{4,}"; // 腾讯QQ号, 腾讯QQ号从10000开始
+	/**
+	 * 常用正则表达式：匹配负整数
+	 */
+	public final static String regExp_integer_4 = "^-[0-9]*[1-9][0-9]*$";
 	
-	public final static String regExp_postbody = "[1-9]\\d{5}(?!\\d)"; // 匹配中国邮政编码
-	
-	public final static String regExp_idCard = "\\d{15} ?\\d{18}"; // 匹配身份证, 中国的身份证为15位或18位
+	/**
+	 * 常用正则表达式：匹配整数
+	 */
+	public final static String regExp_integer_5 = "^-?\\d+$";
 
-	public final static String regExp_ip = "\\d+\\.\\d+\\.\\d+\\.\\d+";//IP
+	/**
+	 * 常用正则表达式：匹配非负浮点数（正浮点数 + 0）
+	 */
+	public final static String regExp_float_1 = "^\\d+(\\.\\d+)?$";
+	
+	/**
+	 * 常用正则表达式：匹配正浮点数
+	 */
+	public final static String regExp_float_2 = "^(([0-9]+\\.[0-9]*[1-9][0-9]*) ?([0-9]*[1-9][0-9]*\\.[0-9]+) ?([0-9]*[1-9][0-9]*))$"; 
+	
+	/**
+	 * 常用正则表达式：匹配非正浮点数（负浮点数 + 0）
+	 */
+	public final static String regExp_float_3 = "^((-\\d+(\\.\\d+)?) ?(0+(\\.0+)?))$";
+	
+	/**
+	 * 常用正则表达式：匹配负浮点数
+	 */
+	public final static String regExp_float_4 = "^(-(([0-9]+\\.[0-9]*[1-9][0-9]*) ?([0-9]*[1-9][0-9]*\\.[0-9]+) ?([0-9]*[1-9][0-9]*)))$";
+	
+	/**
+	 * 常用正则表达式：匹配浮点数
+	 */
+	public final static String regExp_float_5 = "^(-?\\d+)(\\.\\d+)?$";
+
+	/**
+	 * 常用正则表达式：匹配由26个英文字母组成的字符串
+	 */
+	public final static String regExp_letter_1 = "^[A-Za-z]+$";
+	
+	/**
+	 * 常用正则表达式：匹配由26个英文字母的大写组成的字符串
+	 */
+	public final static String regExp_letter_2 = "^[A-Z]+$";
+	
+	/**
+	 * 常用正则表达式：匹配由26个英文字母的小写组成的字符串
+	 */
+	public final static String regExp_letter_3 = "^[a-z]+$";
+	
+	/**
+	 * 常用正则表达式：匹配由数字和26个英文字母组成的字符串
+	 */
+	public final static String regExp_letter_4 = "^[A-Za-z0-9]+$";
+	
+	/**
+	 * 常用正则表达式：匹配由数字、26个英文字母或者下划线组成的字符串
+	 */
+	public final static String regExp_letter_5 = "^\\w+$";
+
+	/**
+	 * 常用正则表达式：匹配email地址
+	 */
+	public final static String regExp_email = "^[\\w-]+(\\.[\\w-]+)*@[\\w-]+(\\.[\\w-]+)+$";
+
+	/**
+	 * 常用正则表达式：匹配url
+	 */
+	public final static String regExp_url_1 = "^[a-zA-z]+://(\\w+(-\\w+)*)(\\.(\\w+(-\\w+)*))*(\\?\\S*)?$";
+	
+	/**
+	 * 常用正则表达式：匹配url
+	 */
+	public final static String regExp_url_2 = "[a-zA-z]+://[^\\s]*";
+
+	/**
+	 * 常用正则表达式：匹配中文字符
+	 */
+	public final static String regExp_chinese_1 = "[\\u4e00-\\u9fa5]";
+	
+	/**
+	 * 常用正则表达式：匹配双字节字符(包括汉字在内)
+	 */
+	public final static String regExp_chinese_2 = "[^\\x00-\\xff]"; 
+
+	/**
+	 * 常用正则表达式：匹配空行
+	 */
+	public final static String regExp_line = "\\n[\\s ? ]*\\r";
+
+	/**
+	 * 常用正则表达式：匹配HTML标记
+	 */
+	public final static String regExp_html_1 = "/ <(.*)>.* <\\/\\1> ? <(.*) \\/>/";
+	
+	/**
+	 * 常用正则表达式：匹配首尾空格
+	 */
+	public final static String regExp_startEndEmpty = "(^\\s*) ?(\\s*$)";
+
+	/**
+	 * 常用正则表达式：匹配帐号是否合法(字母开头，允许5-16字节，允许字母数字下划线)
+	 */
+	public final static String regExp_accountNumber = "^[a-zA-Z][a-zA-Z0-9_]{4,15}$"; 
+
+	/**
+	 * 常用正则表达式：匹配国内电话号码，匹配形式如 0511-4405222 或 021-87888822
+	 */
+	public final static String regExp_telephone = "\\d{3}-\\d{8} ?\\d{4}-\\d{7}";
+
+	/**
+	 * 常用正则表达式：腾讯QQ号, 腾讯QQ号从10000开始
+	 */
+	public final static String regExp_qq = "[1-9][0-9]{4,}";
+
+	/**
+	 * 常用正则表达式：匹配中国邮政编码
+	 */
+	public final static String regExp_postbody = "[1-9]\\d{5}(?!\\d)";
+
+	/**
+	 * 常用正则表达式：匹配身份证, 中国的身份证为15位或18位
+	 */
+	public final static String regExp_idCard = "\\d{15} ?\\d{18}";
+
+	/**
+	 * 常用正则表达式：IP
+	 */
+	public final static String regExp_ip = "\\d+\\.\\d+\\.\\d+\\.\\d+";
 	
 	/**
 	 * 字符编码
