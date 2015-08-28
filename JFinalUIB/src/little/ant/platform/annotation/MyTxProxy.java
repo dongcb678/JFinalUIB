04efa6db09bfc2cd2f2be791d708e19e642fb589
13995531140 <13995531140@163.com>
2015-08-28 16:13:39 +0800

    编写文档顺带整理代码注释

--- JFinalUIB/src/little/ant/platform/annotation/MyTxProxy.java
+++ JFinalUIB/src/little/ant/platform/annotation/MyTxProxy.java
@@ -12,7 +12,8 @@ import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
 
 /**
- * 事务代理
+ * service层cglib事务代理
+ * @author 董华健  dongcb678@163.com
  */
 public class MyTxProxy implements MethodInterceptor {
 
@@ -24,8 +25,10 @@ public class MyTxProxy implements MethodInterceptor {
 
 	@SuppressWarnings("unchecked")
 	public static <T> T newProxy(Class<T> targetClass) {
-		if (null == targetClass)
+		if (null == targetClass){
 			return null;
+		}
+			
 		Object proxy = null;
 		try {
 			T t = targetClass.newInstance();
@@ -33,7 +36,6 @@ public class MyTxProxy implements MethodInterceptor {
 			en.setSuperclass(targetClass);
 			en.setCallback(new MyTxProxy(t));
 			proxy = en.create();
-			System.out.println("创建代理：" + targetClass.getName());
 		} catch (InstantiationException e) {
 			e.printStackTrace();
 		} catch (IllegalAccessException e) {
@@ -45,44 +47,49 @@ public class MyTxProxy implements MethodInterceptor {
 	@Override
 	public Object intercept(Object proxy, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
 		Object result = null;
-		if (method.isAnnotationPresent(MyTx.class)) {
+		if (method.isAnnotationPresent(MyTx.class)) { // 需要事务控制
 			TxInvoke invoke = new TxInvoke(this.target, method, args);
 			Db.tx(invoke);
 			result = invoke.getResult();
-		} else {
+		} else { // 不需要事务
 			result = method.invoke(this.target, args);
 		}
 		return result;
 	}
 
-	private class TxInvoke implements IAtom {
-		private Object target = null;
-		private Method method = null;
-		private Object[] args = null;
-		private Object result = null;
+}
 
-		public TxInvoke(Object target, Method method, Object[] args) {
-			this.target = target;
-			this.method = method;
-			this.args = args;
+/**
+ * 数据库事务控制
+ * @author 董华健  dongcb678@163.com
+ */
+class TxInvoke implements IAtom {
+	
+	private Object target = null;
+	private Method method = null;
+	private Object[] args = null;
+	private Object result = null;
+
+	public TxInvoke(Object target, Method method, Object[] args) {
+		this.target = target;
+		this.method = method;
+		this.args = args;
+	}
+
+	@Override
+	public boolean run() throws SQLException {
+		boolean flag = false;
+		try {
+			this.result = this.method.invoke(this.target, args);
+			flag = true;
+		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			throw new SQLException(e); // 异常信息继续往外抛，否则在外层无法捕获异常信息
 		}
+		return flag;
+	}
 
-		@Override
-		public boolean run() throws SQLException {
-			boolean flag = false;
-			try {
-				this.result = this.method.invoke(this.target, args);
-				flag = true;
-			} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-				throw new SQLException(e);
-			}
-			return flag;
-		}
-
-		public Object getResult() {
-			return this.result;
-		}
-
+	public Object getResult() {
+		return this.result;
 	}
 
 }
