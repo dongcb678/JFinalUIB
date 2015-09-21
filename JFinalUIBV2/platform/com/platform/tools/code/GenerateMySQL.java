2d35644497eda98946155f995ed1796ff2327d8b
13995531140 <13995531140@163.com>
2015-09-21 10:53:16 +0800

    编写mysql代码生成器

---
+++ JFinalUIBV2/platform/com/platform/tools/code/GenerateMySQL.java
@@ -0,0 +1,149 @@
+package com.platform.tools.code;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
+import com.jfinal.plugin.activerecord.CaseInsensitiveContainerFactory;
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Record;
+import com.jfinal.plugin.activerecord.dialect.MysqlDialect;
+import com.jfinal.plugin.druid.DruidPlugin;
+import com.jfinal.plugin.ehcache.EhCachePlugin;
+import com.platform.plugin.SqlXmlPlugin;
+import com.platform.tools.ToolSqlXml;
+import com.platform.tools.ToolString;
+
+/**
+ * 定制MySQL下的代码生成
+ * @author 董华健
+ */
+public class GenerateMySQL extends GenerateBase {
+
+	private static Logger log = Logger.getLogger(GenerateMySQL.class);
+	
+	/**
+	 * 循环生成文件
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		GenerateBase base = new GenerateMySQL();
+		for (int i = 0; i < tableArr.length; i++) {
+			// 数据源名称
+			String dataSource = tableArr[i][0]; 
+			// 表名
+			String tableName = tableArr[i][1]; 
+			// 主键
+			String pkName = tableArr[i][2]; 
+			// 类名
+			String className = tableArr[i][3]; 
+			// 类名首字母小写
+			String classNameSmall = ToolString.toLowerCaseFirstOne(className); 
+			
+			// 1.生成sql文件
+			base.sql(classNameSmall, tableName); 
+			
+			// 2.生成model
+			base.model(className, classNameSmall, dataSource, tableName, pkName); 
+			
+			// 3.生成validator
+			base.validator(className, classNameSmall); 
+			
+			// 4.生成controller
+			base.controller(className, classNameSmall, tableName); 
+			
+			// 5.生成service
+			base.service(className, classNameSmall); 
+
+			// 6.生成DTO，还没有处理数据库字段类型到java数据类型的对应转换
+			//base.dto(base, className, classNameSmall, dataSource, tableName); 
+			
+			// 7.生成视图文件
+			//base.form(classNameSmall, tableName);
+			//base.view(classNameSmall, tableName);
+		}
+		
+		System.exit(0);
+	}
+
+	@Override
+	public List<TableColumnDto> getColunm(String tableName) {
+		log.info("configPlugin 配置Druid数据库连接池连接属性");
+		DruidPlugin druidPlugin = new DruidPlugin(
+				"jdbc:mysql://127.0.0.1:3306/information_schema?characterEncoding=UTF-8"
+				+ "&autoReconnect=true&failOverReadOnly=false&zeroDateTimeBehavior=convertToNull", 
+				"root", 
+				"678789", 
+				"com.mysql.jdbc.Driver");
+
+		log.info("configPlugin 配置ActiveRecord插件");
+		ActiveRecordPlugin arp = new ActiveRecordPlugin(druidPlugin);
+		arp.setDevMode(true); // 设置开发模式
+		arp.setShowSql(true); // 是否显示SQL
+		arp.setContainerFactory(new CaseInsensitiveContainerFactory(true));// 大小写不敏感
+		arp.setDialect(new MysqlDialect());
+		
+		druidPlugin.start();
+		arp.start();
+		
+		log.info("EhCachePlugin EhCache缓存");
+		EhCachePlugin ehCachePlugin = new EhCachePlugin();
+		ehCachePlugin.start();
+
+		log.info("SqlXmlPlugin 解析并缓存 xml sql");
+		SqlXmlPlugin sqlXmlPlugin = new SqlXmlPlugin();
+		sqlXmlPlugin.start();
+		
+		List<TableColumnDto> list = new ArrayList<TableColumnDto>();
+		
+		String tableDesc = Db.findFirst(ToolSqlXml.getSql("platform.mysql.getTables"), "jfinaluibv2", tableName).getStr("table_COMMENT");
+		List<Record> listColumn = Db.find(ToolSqlXml.getSql("platform.mysql.getColumns"), "jfinaluibv2", tableName);
+		
+		for (Record record : listColumn) {
+			String column_name = record.getStr("column_name");
+			String column_type = record.getStr("column_type");
+			String character_maximum_length = String.valueOf(record.getBigInteger("CHARACTER_MAXIMUM_LENGTH"));
+			String column_comment = record.getStr("COLUMN_COMMENT");
+
+			// 需要跳过的字段
+			if("xxx".equals(column_name) || "yyy".equals(column_name) || "zzz".equals(column_name)){
+				continue;
+			}
+			
+			TableColumnDto table = new TableColumnDto();
+			table.setTable_name(tableName);
+			table.setTable_desc(tableDesc);
+			
+			table.setColumn_name(column_name);
+			table.setColumn_type(column_type);
+			table.setColumn_length(character_maximum_length);
+			table.setColumn_desc(column_comment);
+			
+			list.add(table);
+		}
+		
+		return list;
+	}
+
+	@Override
+	public String dbTypeToJava(String columnType){
+		String javaDataType = null;
+		if(columnType.indexOf("char") != -1 || columnType.indexOf("text") != -1){
+			javaDataType = "String";
+			
+		} else if(columnType.indexOf("int") != -1){
+			javaDataType = "long";
+			
+		} else if(columnType.indexOf("time") != -1 || columnType.indexOf("date") != -1){
+			javaDataType = "Date";
+			
+		} else if(columnType.indexOf("byte") != -1 || columnType.indexOf("text") != -1){
+			javaDataType = "byte[]";
+		}
+		
+		return javaDataType;
+	}
+}
