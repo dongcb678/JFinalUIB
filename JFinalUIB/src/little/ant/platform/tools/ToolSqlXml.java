5ca4bcaee2477ddeac226c9069633f1102b02a83
13995531140 <13995531140@163.com>
2015-10-21 11:41:53 +0800

    代码优化

--- JFinalUIB/src/little/ant/platform/tools/ToolSqlXml.java
+++ JFinalUIB/src/little/ant/platform/tools/ToolSqlXml.java
@@ -99,9 +99,13 @@ public class ToolSqlXml {
 			if(param.get(key) == null){
 				break;
 			}
-			String value = (String) param.get(key);
-			value = value.replace("'", "").replace(";", "").replace("--", "");
-			sql = sql.replace("#" + key + "#", value);
+
+			Object paramValue = param.get(key);
+			if(paramValue instanceof String){
+				String value = (String) paramValue;
+				value = value.replace("'", "").replace(";", "").replace("--", "");
+				sql = sql.replace("#" + key + "#", value);
+			}
 		}
 		
         return sql.replaceAll("[\\s]{2,}", " ");
