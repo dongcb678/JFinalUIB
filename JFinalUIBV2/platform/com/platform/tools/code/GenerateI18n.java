cab2c8065e43b4d066e4c1f97c02111e149cadac
13995531140 <13995531140@163.com>
2015-12-09 14:14:47 +0800

    优化国际化key代码生成

--- JFinalUIBV2/platform/com/platform/tools/code/GenerateI18n.java
+++ JFinalUIBV2/platform/com/platform/tools/code/GenerateI18n.java
@@ -11,10 +11,7 @@ import java.util.Properties;
 
 import org.apache.log4j.Logger;
 
-import com.jfinal.kit.PropKit;
 import com.platform.config.run.ConfigCore;
-import com.platform.constant.ConstantInit;
-import com.platform.plugin.I18NPlugin;
 import com.platform.tools.ToolDirFile;
 
 /**
@@ -30,23 +27,14 @@ public class GenerateI18n  {
     	new ConfigCore();
     	log.info("启动ConfigCore end ......");
 
-		String fileNamePrefix = PropKit.get(ConstantInit.config_i18n_filePrefix);
-		String fileName = null;
-		String classesPath = ToolDirFile.getClassesPath();
-		
-		String language = I18NPlugin.i18n_local_zh_CN;
-		fileName = fileNamePrefix + "_" + language + ".properties";// + File.separator + 
+		String propPath = ToolDirFile.getClassesPath() + File.separator + "message_zh_CN.properties";
 		InputStream inputStream = null;
 		try {
-			//inputStream = this.getClass().getClassLoader().getResourceAsStream(filePath);
-			//inputStream = I18NPlugin.class.getResourceAsStream(fileName);// "/init.properties"
-			inputStream = new FileInputStream(new File(classesPath  + File.separator +  fileName));
+			inputStream = new FileInputStream(new File(propPath));
 			
 			Properties properties = new Properties();
 			properties.load(inputStream);
 
-			log.debug("加载国际化资源文件：" + fileName); 
-			
 			Map<String, Object> paramMap = new HashMap<String, Object>();
 			paramMap.put("package", "com.platform.constant");
 			paramMap.put("className", "ConstantI18n");
@@ -60,9 +48,9 @@ public class GenerateI18n  {
 			}
 			paramMap.put("i18nMap", i18nMap);
 
-			String filePath = System.getProperty("user.dir") + "/platform/com/platform/constant/ConstantI18n.java";
+			String outPath = System.getProperty("user.dir") + "/platform/com/platform/constant/ConstantI18n.java";
 			
-			GenerateBase.createFileByTemplete("i18n.html", paramMap, filePath);
+			GenerateBase.createFileByTemplete("i18n.html", paramMap, outPath);
 		} catch (Exception exception) {
 			log.info("加载properties失败！...");
 		} finally {
