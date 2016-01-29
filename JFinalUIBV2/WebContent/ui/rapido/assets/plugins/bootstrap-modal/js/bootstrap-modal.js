bb28620323b3b15a0ffcb4ad47443ade51e19858
13995531140 <13995531140@163.com>
2016-01-29 14:43:33 +0800

    修复弹出层样式显示问题

--- JFinalUIBV2/WebContent/ui/rapido/assets/plugins/bootstrap-modal/js/bootstrap-modal.js
+++ JFinalUIBV2/WebContent/ui/rapido/assets/plugins/bootstrap-modal/js/bootstrap-modal.js
@@ -143,7 +143,6 @@
 					.addClass('modal-overflow');
 			} else {
 				this.$element
-					.css('margin-top', 0 - this.$element.height() / 2)
 					.css('margin-top', (0 - this.$element.height() / 2) < 0 ? 0 : (0 - this.$element.height()) / 2)
 					.removeClass('modal-overflow');
 			}
