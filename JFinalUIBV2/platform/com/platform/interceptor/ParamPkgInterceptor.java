9a833836b10a891a47a4fbf541a9561e5a94a936
13995531140 <13995531140@163.com>
2016-03-15 14:50:10 +0800

    分页查询条件值中存在%时需要转义

--- JFinalUIBV2/platform/com/platform/interceptor/ParamPkgInterceptor.java
+++ JFinalUIBV2/platform/com/platform/interceptor/ParamPkgInterceptor.java
@@ -104,6 +104,10 @@ public class ParamPkgInterceptor implements Interceptor {
 			name = paramNames.nextElement();
 			value = controller.getPara(name);
 			if (name.startsWith(ConstantWebContext.request_query) && null != value && !value.trim().isEmpty()) {// 查询参数分拣
+				// 分页查询条件值中存在%时需要转义
+				if(value.indexOf("%") != -1){
+					value = value.replace("%", "\\%");
+				}
 				log.debug("分页，查询参数：name = " + name + " value = " + value);
 				key = name.substring(7);
 				if(ToolString.regExpVali(key, ToolString.regExp_letter_5)){
