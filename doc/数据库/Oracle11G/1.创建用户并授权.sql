94845315c6a0e746b84a08865f97158a73303556
dongcb678 <dongcb678@163.com>
2016-07-04 14:20:54 +0800

    oracle

--- doc/数据库/Oracle11G/1.创建用户并授权.sql
+++ doc/数据库/Oracle11G/1.创建用户并授权.sql
@@ -2,27 +2,27 @@
 -- 使用系统管理员账号进行操作
 
 --删除用户
-drop user jfinaluibv2 cascade;
-drop tablespace jfinaluibv2_temp including contents and datafiles;
-drop tablespace jfinaluibv2_data including contents and datafiles;
+drop user jfinaluibv3 cascade;
+drop tablespace jfinaluibv3_temp including contents and datafiles;
+drop tablespace jfinaluibv3_data including contents and datafiles;
 
 --创建临时表空间
-create temporary tablespace jfinaluibv2_temp
-tempfile 'D:\Program Files\jfinaluibv2.dbf'
+create temporary tablespace jfinaluibv3_temp
+tempfile 'D:\Program Files\jfinaluibv3.dbf'
 size 200m
 autoextend on
 next 50m maxsize 20480m
 extent management local;
 
 --创建表空间
-create tablespace jfinaluibv2_data
+create tablespace jfinaluibv3_data
 logging
-datafile 'D:\Program Files\jfinaluibv2_data.dbf'
+datafile 'D:\Program Files\jfinaluibv3_data.dbf'
 size 200m autoextend on   next 50m maxsize 20480m   
 extent management local;
 
 --创建用户
-create user jfinaluibv2 identified by jfinaluibv2 default tablespace jfinaluibv2_data;
+create user jfinaluibv3 identified by jfinaluibv3 default tablespace jfinaluibv3_data;
 
 --用户授权
-grant resource,connect,dba to jfinaluibv2; 
+grant resource,connect,dba to jfinaluibv3; 
