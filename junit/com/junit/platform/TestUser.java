ecb86ddb62e38bd57460ed451300db1d98b1dc8a
dongcb678 <dongcb678@163.com>
2017-02-06 16:04:09 +0800

    测试关联查询，Model是否能带出其他表字段

--- junit/com/junit/platform/TestUser.java
+++ junit/com/junit/platform/TestUser.java
@@ -1,12 +1,16 @@
 package com.junit.platform;
 
+import java.util.Date;
+
 import org.apache.commons.codec.binary.Base64;
 import org.junit.Test;
 
 import com.junit.TestBase;
 import com.platform.mvc.user.User;
+import com.platform.mvc.user.UserInfo;
 import com.platform.mvc.user.UserService;
 import com.platform.plugin.ServicePlugin;
+import com.platform.tools.ToolDateTime;
 import com.platform.tools.security.ToolPbkdf2;
 
 public class TestUser extends TestBase {
@@ -41,6 +45,20 @@ public class TestUser extends TestBase {
 		
 		System.out.println("长度" + Base64.encodeBase64String(salt).length());
 		System.out.println("长度" + Base64.encodeBase64String(encryptedPassword).length());
+	}
+
+	/**
+	 * 测试关联查询，Model是否能带出其他表字段
+	 */
+	@Test
+    public void join(){
+		String sql = " select u.*, ui.birthday "
+				+ " from pt_user u "
+				+ " left join pt_userinfo ui on ui.ids = u.ids "
+				+ " where u.ids = ? ";
+		User user = User.dao.findFirst(sql, "e41d790ca25644dc96ad95c994b33130");
+		Date birthday = user.getDate(UserInfo.column_birthday);
+		System.out.println(ToolDateTime.format(birthday, ToolDateTime.pattern_ymd));
 	}
 	
 }
