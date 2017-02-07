2dd0cd537a8671e395602562df9e6d81ca61c004
dongcb678 <dongcb678@163.com>
2017-02-07 17:45:03 +0800

    comment

--- platform/com/platform/config/Run.java
+++ platform/com/platform/config/Run.java
@@ -9,7 +9,7 @@ public class Run {
 	 * 
 	 * 说明：
 	 * 		1. linux 下非root账户运行端口要>1024
-	 * 		2. idea 中运行webAppDir路径可能需要适当调整
+	 * 		2. idea 中运行去掉最后一个参数
 	 */
 	public static void main(String[] args) {
 		JFinal.start("WebContent", 8899, "/", 5);
